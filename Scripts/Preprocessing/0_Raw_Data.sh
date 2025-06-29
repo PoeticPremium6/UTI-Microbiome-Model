e5ee315b13765b7ccdf08e9bbe67687e1939e8bb
Jonathan Josephs-Spaulding <49744158+PoeticPremium6@users.noreply.github.com>
2025-06-29 12:45:46 +0100

    Create 0_Raw_Data.sh

---
+++ Scripts/Preprocessing/0_Raw_Data.sh
@@ -0,0 +1,2 @@
+All in-house generated sequences were deposited in the NCBI-Sequence Read Archive database (SRP498164). 
+The accession number of the deposited reads is provided in Supplementary Table S1.
